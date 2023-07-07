f77d72917db12ce8eb401788049fa4bec76d480a
LinuXminds <linu@datazoom.io>
2023-07-07 13:36:36 +0530

    final test

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('hello-world')
 @app.route('/')
 def hello():
- return "Iam LinuJose returning this from github actions merged from PR branch!!!\n"
+ return "Iam LinuJose returning this from github actions merged from PR branch final test!!!\n"
 if __name__ == '__main__':
  app.run(host = '0.0.0.0', port = 8080)
