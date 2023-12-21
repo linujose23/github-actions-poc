0edf055f25529ef6a31886feb3b141d36df632c9
LinuXminds <linu@datazoom.com>
2023-12-21 17:59:04 +0530

    test 101

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('hello-world')
 @app.route('/')
 def hello():
- return "This is simple push to repos !!!\n"
+ return "This is simple push to reposs !!!\n"
 if __name__ == '__main__':
  app.run(host = '0.0.0.0', port = 8080)
